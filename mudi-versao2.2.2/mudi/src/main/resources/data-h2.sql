61c4bd62638495c4894c9d45132d722e623fd53a
Thiago Souto <thiagocsouto@outlook.com>
2022-04-18 12:55:31 -0500

    Add files via upload

---
+++ mudi-versao2.2.2/mudi/src/main/resources/data-h2.sql
@@ -0,0 +1,4 @@
+insert into pedido(id, nome, valor, data_entrega, url_produto, url_imagem, descrição) values
+(1, 'Xiaomi Redmi 9', 990.99, PARSEDATETIME('28/01/2019', 'dd/MM/yyyy'), 'https://www.amazon.com.br/Celular-Xiaomi-Redmi-Activ-128Gb/dp/B09GFMHKX2/ref=sr_1_13?crid=2U62CX9FAEOPN&keywords=moto+g20&qid=1649353708&sprefix=moto+g2%2Caps%2C235&sr=8-13&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147', 'https://m.media-amazon.com/images/I/919IyPIfczL._AC_SL1500_.jpg', 'Celular novo da xiaomi')
+;
+  
